f5fd897365730055a2ea7f2202174eb8c5e5da8e
Siswanto <siswantosmik@gmail.com>
2021-03-24 18:08:36 +0700

    implementation  axios in list country

--- pages/component/Home/Map/index.js
+++ pages/component/Home/Map/index.js
@@ -1,4 +1,6 @@
 import styles from './Map.module.css';
+import axios from 'axios';
+import { Component, useState } from 'react'
 import map from '@public/map.png';
 import prev from '@public/prev.png';
 import line from '@public/line.png';
@@ -19,92 +21,159 @@ import downhorizontal from '@public/downhorizontal.png';
 import linehorizontal2 from '@public/linehorizontal2.png';
 
 
-const liveReportList = [
-    {
-        flag: usa,
-        name: 'USA',
-        total: '100,392',
-        status: up
-    },
-    {
-        flag: italy,
-        name: 'Italy',
-        total: '86,498',
-        status: up
-    },
-    {
-        flag: china,
-        name: 'China',
-        total: '81,340',
-        status: down
-    },
-    {
-        flag: spain,
-        name: 'Spain',
-        total: '64,059',
-        status: up
-    },
-    {
-        flag: germany,
-        name: 'Germany',
-        total: '50,871',
-        status: up
-    },
-    {
-        flag: iran,
-        name: 'Iran',
-        total: '32,332',
-        status: up
-    }
-]
+// const liveReportList = [
+//     {
+//         flag: usa,
+//         name: 'USA',
+//         total: '100,392',
+//         status: up
+//     },
+//     {
+//         flag: italy,
+//         name: 'Italy',
+//         total: '86,498',
+//         status: up
+//     },
+//     {
+//         flag: china,
+//         name: 'China',
+//         total: '81,340',
+//         status: down
+//     },
+//     {
+//         flag: spain,
+//         name: 'Spain',
+//         total: '64,059',
+//         status: up
+//     },
+//     {
+//         flag: germany,
+//         name: 'Germany',
+//         total: '50,871',
+//         status: up
+//     },
+//     {
+//         flag: iran,
+//         name: 'Iran',
+//         total: '32,332',
+//         status: up
+//     }
+// ]
 
-function Map() {
-    return(
-        <div className={styles.mapContainer}>
-            <div className={styles.mapNavigatorWrap}>
-                <div className={styles.mapNavigator}>
-                    <div className={styles.mapNavigatorTop}>
-                        <div><img src={zoomin}></img></div>
-                        <div><img src={linehorizontal2}></img></div>
-                        <div><img src={zoomout}></img></div>
-                        <div><img src={linehorizontal2}></img></div>
-                    </div>
-                    <div className={styles.mapNavigatorBottom}>
-                        <div><img src={uphorizontal}></img></div>
-                        <div><img src={downhorizontal}></img></div>
-                    </div>  
-                </div>
-                <div className={styles.searchWrap}>
-                    <img src={search}></img>
-                </div>
-            </div>
-            <div className={styles.mapWrap}>
-                <img src={map}></img>
-            </div>
-            <div className={styles.reportWrap}>
-                <div className={styles.reportList}>
-                    <div className={styles.headList}>
-                        <div>
-                            <h1>Live Reports</h1>
+// function Map() {
+//     return(
+//         <div className={styles.mapContainer}>
+//             <div className={styles.mapNavigatorWrap}>
+//                 <div className={styles.mapNavigator}>
+//                     <div className={styles.mapNavigatorTop}>
+//                         <div><img src={zoomin}></img></div>
+//                         <div><img src={linehorizontal2}></img></div>
+//                         <div><img src={zoomout}></img></div>
+//                         <div><img src={linehorizontal2}></img></div>
+//                     </div>
+//                     <div className={styles.mapNavigatorBottom}>
+//                         <div><img src={uphorizontal}></img></div>
+//                         <div><img src={downhorizontal}></img></div>
+//                     </div>  
+//                 </div>
+//                 <div className={styles.searchWrap}>
+//                     <img src={search}></img>
+//                 </div>
+//             </div>
+//             <div className={styles.mapWrap}>
+//                 <img src={map}></img>
+//             </div>
+//             <div className={styles.reportWrap}>
+//                 <div className={styles.reportList}>
+//                     <div className={styles.headList}>
+//                         <div>
+//                             <h1>Live Reports</h1>
+//                         </div>
+//                         <div className={styles.navList}>
+//                             <div><img src={prev}></img></div>
+//                             <div><img src={line}></img></div>
+//                             <div><img src={next}></img></div>  
+//                         </div>
+//                     </div>
+//                     {liveReportList.map((obj, idx) => 
+//                         <div key={idx} className={styles.cardList}>    
+//                             <div className={styles.flagImg}><img src={obj.flag}></img></div>
+//                             <h1>{obj.name}</h1>
+//                             <h2>{obj.total}</h2>
+//                             <div className={styles.statusImg}><img src={obj.status}></img></div>
+//                         </div> 
+//                     )}
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Map;
+
+class Map extends Component {
+    state = {
+        countries: []
+    }
+
+    componentDidMount(){
+        axios.get('https://6056df7a055dbd0017e84408.mockapi.io/country')
+            .then(res => {
+                const countries = res.data;
+                this.setState({ countries });
+            })
+    }
+
+    render(){
+        return(
+            <div className={styles.mapContainer}>
+                <div className={styles.mapNavigatorWrap}>
+                    <div className={styles.mapNavigator}>
+                        <div className={styles.mapNavigatorTop}>
+                            <div><img src={zoomin}></img></div>
+                            <div><img src={linehorizontal2}></img></div>
+                            <div><img src={zoomout}></img></div>
+                            <div><img src={linehorizontal2}></img></div>
                         </div>
-                        <div className={styles.navList}>
-                            <div><img src={prev}></img></div>
-                            <div><img src={line}></img></div>
-                            <div><img src={next}></img></div>  
+                        <div className={styles.mapNavigatorBottom}>
+                            <div><img src={uphorizontal}></img></div>
+                            <div><img src={downhorizontal}></img></div>
+                        </div>  
+                    </div>
+                    <div className={styles.searchWrap}>
+                        <img src={search}></img>
+                    </div>
+                </div>
+                <div className={styles.mapWrap}>
+                    <img src={map}></img>
+                </div>
+                <div className={styles.reportWrap}>
+                    <div className={styles.reportList}>
+                        <div className={styles.headList}>
+                            <div>
+                                <h1>Live Reports</h1>
+                            </div>
+                            <div className={styles.navList}>
+                                <div className={styles.navPrev}><img src={prev}></img></div>
+                                <div className={styles.navLine}><img src={line}></img></div>
+                                <div className={styles.navNext}><img src={next}></img></div>  
+                            </div>
+                        </div>
+                        <div className={styles.countryWrap}>
+                            {this.state.countries.map((country, idx) =>
+                                <div key={idx} className={styles.cardList}>    
+                                    <div className={styles.flagImg}><img src={country.flag}></img></div>
+                                    <h1>{country.name_country}</h1>
+                                    <h2>{country.total_case}</h2>
+                                    <div className={styles.statusImg}><img src={country.status ? up : down}></img></div>
+                                </div> 
+                            )}
                         </div>
                     </div>
-                    {liveReportList.map((obj, idx) => 
-                        <div key={idx} className={styles.cardList}>    
-                            <div className={styles.flagImg}><img src={obj.flag}></img></div>
-                            <h1>{obj.name}</h1>
-                            <h2>{obj.total}</h2>
-                            <div className={styles.statusImg}><img src={obj.status}></img></div>
-                        </div> 
-                    )}
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Map;
--- pages/component/Home/Subscribe/index.js
+++ pages/component/Home/Subscribe/index.js
@@ -6,10 +6,15 @@ function Subscribe() {
             <h1>
                 Have Question in mind? Let us help you
             </h1>
-            <div className={styles.formSubscribe}>
-                <a>snmonydemo@gmail.com</a>
+            <div className={styles.formSubscribeWrap}>
+                <input className={styles.formSubscribe} type="text" id="fname" name="firstname" placeholder="snmonydemo@gmail.com"></input>
                 <button className={styles.btnSubscribe}>Send</button>
             </div>
+            {/* <div className={styles.formSubscribe}>
+                <input className={styles.inputSubscribe} type="text" id="fname" name="firstname" placeholder="snmonydemo@gmail.com"></input>
+                <a>snmonydemo@gmail.com</a>
+                <button className={styles.btnSubscribe}>Send</button>
+            </div> */}
         </div>
     )
 }
